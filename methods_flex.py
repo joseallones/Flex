be8b4e47dbf9b6fe8f84e5fd8f44b9da75e8d47a
jose <joseallones87@gmail.com>
2020-09-23 13:41:08 +0200

    engadir flexión de adxectivos

--- methods_flex.py
+++ methods_flex.py
@@ -4,6 +4,10 @@ import csv
 
 from methods_io import gardaListadoNunFicheiro
 
+dict_list_subs_gl = []
+dict_list_subs_pt = []
+dict_list_adxs_gl = []
+dict_list_adxs_pt = []
 
 # Función para leer un paquete csv e crear una lista de diccionarios
 def csv_dict_list(variables_file):
@@ -16,36 +20,100 @@ def csv_dict_list(variables_file):
     return dict_list
 
 
+def recupera_diccionario(tipo):
+
+    global dict_list_subs_gl
+    global dict_list_subs_pt
+    global dict_list_adxs_gl
+    global dict_list_adxs_pt
+
+    if(tipo == "nouns_gl"):
+        if dict_list_subs_gl:
+            return dict_list_subs_gl
+        else:
+            dict_list_subs_gl = csv_dict_list("data/nouns_gl.csv")
+            return dict_list_subs_gl
+
+    elif (tipo == "nouns_pt"):
+        if dict_list_subs_pt:
+            return dict_list_subs_pt
+        else:
+            dict_list_subs_pt = csv_dict_list("data/nouns_pt.csv")
+            return dict_list_subs_pt
+
+    elif (tipo == "adxs_pt"):
+        if dict_list_adxs_pt:
+            return dict_list_adxs_pt
+        else:
+            dict_list_adxs_pt = csv_dict_list("data/adxs_pt.csv")
+            return dict_list_adxs_pt
+
+    elif (tipo == "adxs_gl"):
+        if dict_list_adxs_gl:
+            return dict_list_adxs_gl
+        else:
+            dict_list_adxs_gl = csv_dict_list("data/adxs_gl.csv")
+            return dict_list_adxs_gl
+
+    else:
+        return []
+
 # Función para buscar as flexións para un lema
 def search(lemma, lang = "pt",  pos = None, gen = None, num = None):
 
-    dict_list = []
+    dict_list_subs = []
+    dict_list_adx = []
+
     if lang:
         if lang.upper() =="GL":
-            dict_list = csv_dict_list("data/nouns_gl.csv")
-        elif lang.upper() =="PT":
-            dict_list = csv_dict_list("data/nouns_pt.csv")
-    else:
-        dict_list = csv_dict_list("data/nouns_pt.csv")
+            dict_list_subs = recupera_diccionario("nouns_gl")
+            dict_list_adx = recupera_diccionario("adxs_gl")
+        else:
+            dict_list_subs = recupera_diccionario("nouns_pt")
+            dict_list_adx = recupera_diccionario("adxs_pt")
 
-    list = [element for element in dict_list if element['lemma'] == lemma.lower()]
+    listSubs = []
+    listAdxs = []
 
-    if pos:
-        list = [element for element in list if element['pos'][0:2] == pos.upper()]
+    if(not pos or (pos and pos.upper().startswith("N")) ):
+        listSubs = [element for element in dict_list_subs if element['lemma'] == lemma.lower()]
 
-    if gen:
-        list = [element for element in list if element['pos'][2] == gen.upper()]
+        if pos:
+            if(len(pos)==1):
+                listSubs = [element for element in listSubs if element['pos'][0:1] == pos.upper()]
+            elif (len(pos) == 2):
+                listSubs = [element for element in listSubs if element['pos'][0:2] == pos.upper()]
 
-    if num:
-        list = [element for element in list if element['pos'][3] == num.upper()]
+        if gen:
+            listSubs = [element for element in listSubs if element['pos'][2] == gen.upper()]
 
-    for element in list:
-        element['gen'] = element['pos'][2]    #NCMP000 --> M
-        element['num'] = element['pos'][3]    #NCMP000 --> P
-        element['pos'] = element['pos'][0:1]  #So a primeira letra (NCMP000 --> N)
+        if num:
+            listSubs = [element for element in listSubs if element['pos'][3] == num.upper()]
 
+        for element in listSubs:
+            element['gen'] = element['pos'][2]    #NCMP000 --> M
+            element['num'] = element['pos'][3]    #NCMP000 --> P
+            element['pos'] = element['pos'][0:1]  #So a primeira letra (NCMP000 --> N)
 
-    return list
+    if (not pos or (pos and pos.upper().startswith("A")) ):
+        listAdxs = [element for element in dict_list_adx if element['lemma'] == lemma.lower()]
+
+        if pos:
+            listAdxs = [element for element in listAdxs if element['pos'][0:1] == pos.upper()]
+
+        if gen:
+            listAdxs = [element for element in listAdxs if element['pos'][2] == gen.upper()]
+
+        if num:
+            listAdxs = [element for element in listAdxs if element['pos'][3] == num.upper()]
+
+        for element in listAdxs:
+            element['gen'] = element['pos'][3]  # AQ0FS --> F
+            element['num'] = element['pos'][4]  # AQ0FS --> S
+            element['pos'] = element['pos'][0:1]  # So a primeira letra (AQ0FS --> A)
+
+    listTotal = listSubs + listAdxs
+    return listTotal
 
 
 # Función para buscar as flexións para un "paquete"
@@ -74,7 +142,23 @@ def obterFlexionsPaquete(infoPaquete, lang, rutaFicheiroTermosSenFlexions):
         for lema in lemas:  # Para cada lema
 
             lema = lema.replace("_", " ")
-            resultadoAux = search(lemma=lema, lang=lang)
+
+            pos = ""
+            if dict['ili'][-1].lower() == 'a':
+                pos = 'A'
+                print(pos + " " + dict['ili'])
+            elif dict['ili'][-1].lower() == 'n':
+                pos = 'NC'
+            else:
+                print("ILI sin terminación " + dict['ili'])
+
+            resultadoAux = []
+            if pos:
+                resultadoAux = search(lemma=lema, lang=lang, pos = pos)
+
+            else:  #se pos (ili) non definido non se filtra por pos
+                resultadoAux = search(lemma=lema, lang=lang)
+
             if resultadoAux:
                 for r in resultadoAux:
                     r['ili'] = dict['ili']
@@ -92,3 +176,5 @@ def obterFlexionsPaquete(infoPaquete, lang, rutaFicheiroTermosSenFlexions):
     gardaListadoNunFicheiro(termos_sen_flexions, rutaFicheiroTermosSenFlexions)
 
     return listadoResultadosFlexions
+
+
